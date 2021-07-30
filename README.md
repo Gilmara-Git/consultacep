7592d4faf57d541919fb900313d77cbe95007cb8
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2021-07-30 07:16:03 -0400

    Adjustments

---
+++ README.md
@@ -0,0 +1,40 @@
+
+<h1 align="center">Consulta Cep</h1>
+
+<h1 align="center"><img src="https://ik.imagekit.io/cnbmdh4b9w/ezgif.com-gif-maker__19__o1owY8AbF.gif?updatedAt=1627610754941"></h1>
+
+## About
+**Consulta Cep** is an application that searches "Zip code" (Cep) in Brazil using an API.
+<p>An ajax object was created using <b>XMLHttpRequest)()</b>
+<p>We can either get the response in <b>XML</b> or <b>JSON.</b></p>
+
+<p>I worked with <b>JSON</b>, then, manipulated the data and fed it into the application.</p>
+<p>Validations were applied in case data is not found.</p>
+
+
+## Motivation
+Learn how to create an API request using Ajax, add the event "readystatechange" to it, analyze its "stauts", "readyState" and get its "response".
+
+## API used:
+[ApiCep](https://apicep.com/api-de-consulta/)
+
+## Technologies used
+- Html5 
+- Css3 
+- JS
+- Ajax
+
+## To clone: 
+
+- Save it on C: drive
+- Open your browser 
+
+
+
+
+
+
+
+
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -1,120 +1,113 @@
-(function () {
+(function (DOM) {
   "use strict";
 
-  function DOM(string) {
-    this.element = document.querySelectorAll(string);
-  }
+  function app() {
+    debugger
+    let $form = new DOM("#form");
+    let $cepInput = new DOM("#form form input");
+    let $addressField = new DOM('[data-js="street"]');
+    let $districtField = new DOM('[data-js="district"]');
+    let $cityField = new DOM('[data-js="city"]');
+    let $stateField = new DOM('[data-js="state"]');
+    let $cepField = new DOM('[data-js="cep"]');
+    let $textArea = new DOM("textArea");
 
-  DOM.prototype.get = function get() {
-    return this.element;
-  };
+    $form.on("submit", handleSubmitFormCep, false);
+    let ajax = new XMLHttpRequest();
 
-  DOM.prototype.on = function (event, handleClick) {
-    Array.prototype.forEach.call(this.element, function (element) {
-      element.addEventListener(event, handleClick, false);
-    });
-  };
+    function handleSubmitFormCep(event) {
+      event.preventDefault();
+      let url = getURL();
 
-  let $form = new DOM("#form");
-  let $cepInput = new DOM("#form form input");
-  let $addressField = new DOM('[data-js="street"]');
-  let $districtField = new DOM('[data-js="district"]');
-  let $cityField = new DOM('[data-js="city"]');
-  let $stateField = new DOM('[data-js="state"]');
-  let $cepField = new DOM('[data-js="cep"]');
-  let $textArea = new DOM("textArea");
-  console.log($textArea);
-
-  $form.on("submit", handleSubmitFormCep, false);
-  let ajax = new XMLHttpRequest();
-
-  function handleSubmitFormCep(event) {
-    event.preventDefault();
-    let url = getURL();
-
-    ajax.open("GET", url);
-    ajax.send();
-    getMessage("loading");
-    ajax.addEventListener("readystatechange", getAjaxResponse, false);
-  }
-
-  function getURL() {
-    return replaceCep("https://ws.apicep.com/cep/[CEP].json");
-  }
-
-  function clearCep() {
-    return $cepInput.get()[0].value.replace(/\D/g, "");
-  }
-
-  function getAjaxResponse() {
-    if (isRequestOk()){
-      getMessage("success")      
-      fillCepFields();
-    } 
-  }
-
-  function isRequestOk() {
-    return ajax.readyState === 4 && ajax.status === 200;
-  }
-
-  function fillCepFields() {
-    
-    let data = parseData();
-    console.log(data)
-  
-    if(data.message === 'Blocked by flood'){
-      getMessage("tryAgain");
-      data = clearFields();
-     
-    }
-  
-    if (data.status === 400 || data.ok === false) {
-      getMessage("error")
-      data = clearFields();
+      ajax.open("GET", url);
+      ajax.send();
+      getMessage("loading");
+      ajax.addEventListener("readystatechange", getAjaxResponse, false);
     }
 
-    $addressField.get()[0].value = data.address;
-    $districtField.get()[0].value = data.district;
-    $cityField.get()[0].value = data.city;
-    $stateField.get()[0].value = data.state;
-    $cepField.get()[0].value = data.code;
-  }
-
-  function clearFields() {
-    return {
-      address: "-",
-      district: "-",
-      city: "-",
-      state: "-",
-      code: "-",
-    };
-  }
-
-  function parseData() {   
-    let result;   
-    try {
-      result = JSON.parse(ajax.responseText);
-    } catch (e) {
-      result;
+    function getURL() {
+      return replaceCep("https://ws.apicep.com/cep/[CEP].json");
     }
-    return result;
-  }
 
-  function getMessage(type) {
+    function clearCep() {
+      return $cepInput.get()[0].value.replace(/\D/g, "");
+    }
+
+    function getAjaxResponse() {
+      if (isRequestOk()) {
+        getMessage("success");
+        fillCepFields();
+      }
+    }
+
+    function isRequestOk() {
+      return ajax.readyState === 4 && ajax.status === 200;
+    }
+
+    function fillCepFields() {
+      let data = parseData();
+      console.log(data);
+
+      if (data.message === "Blocked by flood") {
+        getMessage("tryAgain");
+        data = clearFields();
+      }
+
+      if (data.status === 400 || data.ok === false) {
+        getMessage("error");
+        data = clearFields();
+      }
+
+      $addressField.get()[0].value = data.address;
+      $districtField.get()[0].value = data.district;
+      $cityField.get()[0].value = data.city;
+      $stateField.get()[0].value = data.state;
+      $cepField.get()[0].value = data.code;
+    }
+
+    function clearFields() {
+      return {
+        address: "-",
+        district: "-",
+        city: "-",
+        state: "-",
+        code: "-",
+      };
+    }
+
+    function parseData() {
+      let result;
+      try {
+        result = JSON.parse(ajax.responseText);
+      } catch (e) {
+        result;
+      }
+      return result;
+    }
+
+    function getMessage(type) {
       let message = {
-      loading: replaceCep("Buscando informações para o CEP...[CEP]:"),
-      success: replaceCep("Abaixo, endereço referente ao CEP [CEP]: "),
-      error: replaceCep("Não encontramos o endereço para o CEP [CEP]"),
-      tryAgain: "Não possível completar a busca no momento.\nTente novamente mais tarde!"
+        loading: replaceCep("Buscando informações para o CEP...[CEP]:"),
+        success: replaceCep("Abaixo, endereço referente ao CEP [CEP]: "),
+        error: replaceCep("Não encontramos o endereço para o CEP [CEP]"),
+        tryAgain:
+          "Não possível completar a busca no momento.\nTente novamente mais tarde!",
+      };
+      return ($textArea.get()[0].value = message[type]);
+    }
+
+    function replaceCep(message) {
+      return message
+        .replace("[CEP]", clearCep())
+        .replace(/(\d{5})(\d+)/g, "$1-$2");
+    }
+
+    return {
+      getMessage: getMessage,
+      replaceCep: replaceCep,
     };
-    return $textArea.get()[0].value =  message[type];
-   
   }
 
-  function replaceCep(message) {
-    return message.replace("[CEP]", clearCep()).replace(
-      /(\d{5})(\d+)/g,
-      "$1-$2"
-    );
-  }
-})();
+  window.app = app(); // estamos executando aqui para retornar somente os metodos no 'return'
+  app();
+})(window.DOM, document);
